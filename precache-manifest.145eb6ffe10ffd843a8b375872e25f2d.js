6a6aaf2dd27954e4ee7a28a6bded1cb55117fa89
Anshu S Panda <anshuspanda@gmail.com>
2019-08-12 00:48:41 +0530

    Updates

---
+++ precache-manifest.145eb6ffe10ffd843a8b375872e25f2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf9fcf4ea9681bd17dde11466c0a71f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ea11ceaac1ab468ea0",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "65a1e4ebb1499be24cbf",
+    "url": "/static/js/2.386a3fdc.chunk.js"
+  },
+  {
+    "revision": "97ea11ceaac1ab468ea0",
+    "url": "/static/js/main.a8c60f8e.chunk.js"
+  },
+  {
+    "revision": "b99d4eac875820cfca5a",
+    "url": "/static/js/runtime~main.f3c983a4.js"
+  }
+]);
